678439070fb63431089dfd11a69509794d39b0c6
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-07 22:11:34 -0300

    feat: add styled component

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Open up app/index.tsx to start working on your app!</Text>
+    <Container>
       <StatusBar style="auto" />
-    </View>
+      <Text>This is the HomePage</Text>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  background-color: red;
+  align-items: center;
+  justify-content: center;
+`;
