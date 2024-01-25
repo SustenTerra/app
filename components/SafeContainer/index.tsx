3abb918d06b275420cd71beb04f20c6913f2a592
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-25 11:16:26 -0300

    feat: add styling to safe view

--- components/SafeContainer/index.tsx
+++ components/SafeContainer/index.tsx
@@ -1,11 +1,14 @@
+import { StyleProp, ViewStyle } from 'react-native';
+
 import { Container } from './styles';
 
 interface SafeContainerProps {
   children: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
 }
 
-function SafeContainer({ children }: SafeContainerProps) {
-  return <Container>{children}</Container>;
+function SafeContainer({ children, style }: SafeContainerProps) {
+  return <Container style={style}>{children}</Container>;
 }
 
 export default SafeContainer;
