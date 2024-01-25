704d491c24ef2c56639a50f6efe9ee1790afa156
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-25 17:10:01 -0300

    fix: unmatched page only redirects to home

--- app/[...unmatched].tsx
+++ app/[...unmatched].tsx
@@ -1,29 +1,5 @@
-import { router } from 'expo-router';
-import { useCallback } from 'react';
-import styled from 'styled-components/native';
-
-import Text from '@/components/Text';
+import { Redirect } from 'expo-router';
 
 export default function Unmatched() {
-  const redirectToHome = useCallback(() => {
-    if (router.canGoBack()) {
-      router.back();
-      return;
-    }
-
-    router.replace('/');
-  }, []);
-
-  return (
-    <Container onLayout={redirectToHome}>
-      <Text>Essa página não existe...</Text>
-    </Container>
-  );
+  return <Redirect href="/" />;
 }
-
-const Container = styled.View`
-  flex: 1;
-  background-color: #fff;
-  align-items: center;
-  justify-content: center;
-`;
