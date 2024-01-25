9c9c10052fe1e8e0afc5aed4c9406d053c90efa2
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-25 17:41:25 -0300

    feat: add font options

--- components/Text/index.tsx
+++ components/Text/index.tsx
@@ -1,14 +1,39 @@
 import { StyleProp, TextStyle } from 'react-native';
 
-import { Container } from './styles';
+import {
+  Container,
+  FontColorSchemeOptions,
+  FontTypeOptions,
+  FontWeightOptions,
+} from './styles';
 
 interface TextProps {
   children?: string | string[];
+  type?: FontTypeOptions;
+  weight?: FontWeightOptions;
+  colorScheme?: FontColorSchemeOptions;
+  size?: number;
+  color?: string;
   style?: StyleProp<TextStyle>;
 }
 
-function Text({ children, style }: TextProps) {
-  return <Container style={style}>{children}</Container>;
+function Text({
+  children,
+  type = 'p',
+  weight = 'regular',
+  colorScheme = 'regular',
+  ...props
+}: TextProps) {
+  return (
+    <Container
+      fontType={type}
+      fontWeight={weight}
+      colorScheme={colorScheme}
+      {...props}
+    >
+      {children}
+    </Container>
+  );
 }
 
 export default Text;
--- components/Text/styles.ts
+++ components/Text/styles.ts
@@ -2,7 +2,33 @@ import styled from 'styled-components/native';
 
 import { moderateScale } from '@/utils/scale';
 
-export const Container = styled.Text`
+export type FontTypeOptions = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
+export type FontWeightOptions = 'bold' | 'regular' | 'light';
+export type FontColorSchemeOptions =
+  | 'primary'
+  | 'secondary'
+  | 'regular'
+  | 'light';
+
+const fontType = {
+  h1: moderateScale(40),
+  h2: moderateScale(30),
+  h3: moderateScale(24),
+  h4: moderateScale(20),
+  h5: moderateScale(16),
+  h6: moderateScale(14),
+  p: moderateScale(12),
+};
+
+interface ContainerProps {
+  size?: number;
+  fontType: FontTypeOptions;
+  fontWeight: FontWeightOptions;
+  colorScheme: FontColorSchemeOptions;
+  color?: string;
+}
+
+export const Container = styled.Text<ContainerProps>`
   font-family: 'InriaSans_400Regular';
-  font-size: ${moderateScale(30)}px;
+  font-size: ${(props) => props.size || fontType[props.fontType]}px;
 `;
