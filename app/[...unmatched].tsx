c318547362f79212e46cc898dbfe0f5d366687c5
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-25 16:56:22 -0300

    feat: add unmatched page

---
+++ app/[...unmatched].tsx
@@ -0,0 +1,29 @@
+import { router } from 'expo-router';
+import { useCallback } from 'react';
+import styled from 'styled-components/native';
+
+import Text from '@/components/Text';
+
+export default function Unmatched() {
+  const redirectToHome = useCallback(() => {
+    if (router.canGoBack()) {
+      router.back();
+      return;
+    }
+
+    router.replace('/');
+  }, []);
+
+  return (
+    <Container onLayout={redirectToHome}>
+      <Text>Redirecionando para a página inicial...</Text>
+    </Container>
+  );
+}
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
