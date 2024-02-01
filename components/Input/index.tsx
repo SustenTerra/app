01baa099044464b840a5613d36ff1eeef2f838b1
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-02-01 07:34:17 -0300

    chore: lint

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,39 +1,40 @@
-import { ReactNode } from 'react';
-import { NativeSyntheticEvent, StyleProp, TextInputChangeEventData, TextStyle } from 'react-native';
-
-import {
-  Container,
-  TextInput
-} from './styles';
-
 import Feather from '@expo/vector-icons/Feather';
+import { ReactNode } from 'react';
+import {
+  NativeSyntheticEvent,
+  StyleProp,
+  TextInputChangeEventData,
+  TextStyle,
+} from 'react-native';
 
-type IconNameOptions =
-  | 'user'
-  | 'mail'
-  | 'message-circle'
-  | 'lock'
-  | 'search';
+import { Container, TextInput } from './styles';
+
+type IconNameOptions = 'user' | 'mail' | 'message-circle' | 'lock' | 'search';
 
 interface InputProps {
-    iconName: IconNameOptions;
-    placeholder: string;
-    value: string;
-    onChange: ((text: string) => void);
-    style?: StyleProp<TextStyle>;
+  iconName: IconNameOptions;
+  placeholder: string;
+  value: string;
+
+  onChange: (text: string) => void;
+  style?: StyleProp<TextStyle>;
 }
 
 function Input({
-    iconName,
-    placeholder,
-    value,
-    onChange,
-    ...props}: InputProps) {
+  iconName,
+  placeholder,
+  value,
+  onChange,
+  ...props
+}: InputProps) {
   return (
     <Container {...props}>
-      {iconName &&
-        <Feather name={iconName} size={24} />}
-      <TextInput placeholder={placeholder} value={value} onChangeText={onChange} />
+      {iconName && <Feather name={iconName} size={24} />}
+      <TextInput
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChange}
+      />
     </Container>
   );
 }
