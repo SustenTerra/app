c1acf861e780ac1bfff5c22fdbbd450308bd7c52
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-02-01 09:21:32 -0300

    feat: add default backend url

--- services/client.ts
+++ services/client.ts
@@ -2,12 +2,21 @@ import { getTokenAsync } from './authStorage';
 
 import { AppClient } from '@/api';
 
+declare const process: {
+  env: {
+    EXPO_PUBLIC_API_URL?: string;
+  };
+};
+
 async function getToken() {
   const token = await getTokenAsync();
   return token ? token : '';
 }
 
+const base_url =
+  process.env.EXPO_PUBLIC_API_URL || 'https://sustentinta.up.railway.app';
+
 export const client = new AppClient({
-  BASE: process.env.EXPO_PUBLIC_API_URL,
+  BASE: base_url,
   TOKEN: getToken,
 });
