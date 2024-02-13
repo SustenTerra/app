30de6e198acb23b26630ef898518861bdf461cd5
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-02-13 15:58:08 -0300

    feat: use effect on get course

--- app/courses/[courseId]/index.tsx
+++ app/courses/[courseId]/index.tsx
@@ -1,15 +1,41 @@
 import { useLocalSearchParams } from 'expo-router';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
 
+import { CourseView } from '@/api';
+import Loading from '@/components/Loading';
 import Text from '@/components/Text';
+import { client } from '@/services/client';
+import { showErrors } from '@/services/errors';
 
 export default function ShowCourse() {
   const { courseId } = useLocalSearchParams();
 
+  const [course, setCourse] = useState<CourseView | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const getCourse = async () => {
+    setLoading(true);
+    try {
+      const courseResponse =
+        await client.courses.getCourseByIdCoursesCourseIdGet(Number(courseId));
+      setCourse(courseResponse);
+    } catch (error) {
+      showErrors(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCourse();
+  }, [courseId]);
+
   return (
     <Container>
-      <Text>ShowCourse</Text>
-      <Text>{String(courseId)}</Text>
+      {!loading && <Loading />}
+
+      {course && <Text>{course.name}</Text>}
     </Container>
   );
 }
