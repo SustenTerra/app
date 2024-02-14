17fb8ac311b35eff892a9f3a92d2d257315cdefb
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-02-14 16:23:11 -0300

    feat: use scrollview only on mobile

--- components/ScrollablePage/index.tsx
+++ components/ScrollablePage/index.tsx
@@ -1,4 +1,7 @@
+import { ScrollView, View } from 'react-native';
 import styled from 'styled-components/native';
+
+import { isWeb } from '@/utils/platform';
 
 interface Props {
   children: React.ReactNode;
@@ -21,7 +24,7 @@ function ScrollablePage({ children }: Props) {
 
 export default ScrollablePage;
 
-const Container = styled.ScrollView`
+const Container = styled(isWeb ? View : ScrollView)`
   flex: 1;
   background-color: ${(props) => props.theme.colors.background};
 `;
