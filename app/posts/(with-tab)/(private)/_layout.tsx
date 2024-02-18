852424038ee7d7711c7e5a08884e54fe536fe94b
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-02-18 20:11:11 -0300

    feat: add private routes for posts

--- app/posts/(with-tab)/(private)/_layout.tsx
+++ app/posts/(with-tab)/(private)/_layout.tsx
@@ -1,19 +1,13 @@
 import { Redirect, Slot } from 'expo-router';
 
-import NavigationBar from '@/components/NavigationBar';
 import { useAuth } from '@/hooks/auth';
 
-export default function ProfileLayout() {
+export default function PrivatePostsLayout() {
   const auth = useAuth();
 
   if (!auth.loading && !auth.user) {
     return <Redirect href="/login" />;
   }
 
-  return (
-    <>
-      <Slot />
-      <NavigationBar />
-    </>
-  );
+  return <Slot />;
 }
